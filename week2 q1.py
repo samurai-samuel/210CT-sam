ad3bd59988e839c67fff2f907184dda0f7a23ad9
samurai-samuel <34069548+samurai-samuel@users.noreply.github.com>
2017-11-30 23:35:20 +0000

    week 2
    possible code for week 2

---
+++ week2 q1.py
@@ -0,0 +1,23 @@
+import operator
+
+
+
+degree1 = input("enter a number a number")
+p1 = input("enter  values")
+degree2 = input("enter a number")
+p2 = input("enetr values")
+
+
+if degree1 > degree2:
+    degree_ans = degree1
+ 
+
+else:
+    degree_ans = degree2
+
+print(degree_ans)   
+
+for d in p1:
+  p_ans = d + p2[:]
+
+  print(p_ans)
